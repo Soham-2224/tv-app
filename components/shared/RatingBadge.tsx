e07f6013e22c7c5e5eac597ae2fb970af1a908b3
soham-2224 <sohambhosle26@gmail.com>
2023-12-22 12:39:10 +0530

    rating badge component created

---
+++ components/shared/RatingBadge.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image"
+
+// --components
+import { Badge } from "@/components/ui/badge"
+
+// --icons--
+import { StarIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+
+type Props = {
+    rating: number
+    isStar?: boolean
+    classNames?: String
+}
+
+const RatingBadge = ({ rating, isStar = false, classNames }: Props) => {
+    return (
+        <Badge
+            variant="secondary"
+            className={cn(
+                "gap-1 px-2 py-1 bg-black/50 text-white hover:bg-black/75",
+                !isStar && "p-0 bg-transparent hover:bg-transparent text-foreground",
+                classNames
+            )}
+        >
+            {isStar ? (
+                <StarIcon
+                    size={16}
+                    className=" text-yellow-400 fill-yellow-400"
+                />
+            ) : (
+                <Image
+                    width={32}
+                    height={16}
+                    src="/assets/imdb.png"
+                    alt="imdb logo"
+                    className=" w-8 h-4"
+                />
+            )}
+            {rating?.toFixed(1)}
+        </Badge>
+    )
+}
+
+export default RatingBadge
