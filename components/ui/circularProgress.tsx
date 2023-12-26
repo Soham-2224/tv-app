17ef94eccf4e8b0214d843c9e675b36b5b122e7a
soham-2224 <sohambhosle26@gmail.com>
2023-12-26 22:15:10 +0530

    circular progress component, getImagePath changes

---
+++ components/ui/circularProgress.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useEffect, useRef } from "react"
+
+// --utils--
+import { cn } from "@/lib/utils"
+
+const CircularProgress = ({vote} :{vote: number}) => {
+
+    const circleRef = useRef<SVGCircleElement>(null)
+
+    useEffect(() => {
+        circleRef?.current?.setAttribute(
+            "style",
+            `stroke-dashoffset: calc(160px - (160px * ${vote?.toFixed(1)}) / 10);`
+        )
+    }, [])
+
+  return (
+      <div className={cn("percent scale-75 lg:scale-95 relative w-[60px] h-[60px] rounded-[50%]")}>
+          <svg className="">
+              <circle
+                  cx="25"
+                  cy="25"
+                  r="25"
+              ></circle>
+              <circle
+                  ref={circleRef}
+                  cx="25"
+                  cy="25"
+                  r="25"
+              ></circle>
+          </svg>
+          <div className="number absolute top-0 left-0 w-full h-full flex justify-center items-center">
+              <h2 className="text-white font-bold text-base">
+                  {vote?.toFixed(1)}
+                  <span className=" text-sm ">%</span>
+              </h2>
+          </div>
+      </div>
+  )
+}
+
+export default CircularProgress
--- lib/getImagePath.ts
+++ lib/getImagePath.ts
@@ -1,8 +1,14 @@
 import { Movie } from "@/typings";
 
-const getImagePath = (data?: Partial<Movie>, isLarge?: boolean) => {
+type Props = {
+    data?: Partial<Movie>
+    isLarge?: boolean
+    isPoster?: boolean
+}
+
+const getImagePath = ({data, isLarge, isPoster} : Props) => {
   return `https://image.tmdb.org/t/p/${data?.backdrop_path && isLarge ? "w1280" : isLarge ? "original" : "w780"}${
-      data?.backdrop_path || data?.poster_path
+      !isLarge && isPoster ? data?.poster_path || data?.backdrop_path : data?.backdrop_path
   }`
 };
 
