1094d8b2830e23bebcf5c8ebf3d6186e153c6644
soham-2224 <sohambhosle26@gmail.com>
2023-12-29 19:05:36 +0530

    image not available issue solved

--- lib/getImagePath.ts
+++ lib/getImagePath.ts
@@ -7,8 +7,11 @@ type Props = {
 }
 
 const getImagePath = ({data, isLarge, isPoster} : Props) => {
+
+  if (!data?.backdrop_path && !data?.poster_path) return "/assets/no-image-placeholder.png"
+
   return `https://image.tmdb.org/t/p/${data?.backdrop_path && isLarge ? "w1280" : isLarge ? "original" : "w780"}${
-      !isLarge && isPoster ? data?.poster_path || data?.backdrop_path : data?.backdrop_path
+      !isLarge && isPoster ? data?.poster_path || data?.backdrop_path : data?.backdrop_path || data?.poster_path
   }`
 };
 
