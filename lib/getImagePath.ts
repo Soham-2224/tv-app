9ae15bdfda0c5726ecfcc0fc3da3b35ce350fa47
soham-2224 <sohambhosle26@gmail.com>
2023-12-29 19:52:23 +0530

    getImagePath func updated

--- lib/getImagePath.ts
+++ lib/getImagePath.ts
@@ -1,16 +1,17 @@
-import { Movie } from "@/typings";
+import { Actor, Movie } from "@/typings";
+import { BASE_IMAGE_PATH } from "./constants";
 
 type Props = {
-    data?: Partial<Movie>
+    data?: Partial<Movie> & Partial<Actor>
     isLarge?: boolean
     isPoster?: boolean
 }
 
 const getImagePath = ({data, isLarge, isPoster} : Props) => {
 
-  if (!data?.backdrop_path && !data?.poster_path) return "/assets/no-image-placeholder.png"
+  if (!data?.backdrop_path && !data?.poster_path ) return "/assets/no-image-placeholder.png"
 
-  return `https://image.tmdb.org/t/p/${data?.backdrop_path && isLarge ? "w1280" : isLarge ? "original" : "w780"}${
+  return `${BASE_IMAGE_PATH}${data?.backdrop_path && isLarge ? "w1280" : isLarge ? "original" : "w780"}${
       !isLarge && isPoster ? data?.poster_path || data?.backdrop_path : data?.backdrop_path || data?.poster_path
   }`
 };
