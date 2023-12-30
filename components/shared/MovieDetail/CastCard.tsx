69e13ece3406b7e57da654c8279b8036beb30b0e
soham-2224 <sohambhosle26@gmail.com>
2023-12-30 11:43:59 +0530

    castCard components updated to accept crew data

--- components/shared/MovieDetail/CastCard.tsx
+++ components/shared/MovieDetail/CastCard.tsx
@@ -1,12 +1,12 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { BASE_IMAGE_PATH } from '@/lib/constants'
 import getImagePath from '@/lib/getImagePath'
-import { Actor } from '@/typings'
+import { Actor, Crew } from '@/typings'
 import React from 'react'
 
-const CastCard = ({data}:{data: Actor}) => {
+const CastCard = ({data}:{data: Partial<Actor> & Partial<Crew>}) => {
 
-    const characterName = data?.name || data?.original_name
+    const characterName = data?.name || data?.original_name || ""
 
   return (
       <div className="flex gap-4 items-center">
@@ -19,7 +19,7 @@ const CastCard = ({data}:{data: Actor}) => {
           </Avatar>
           <div>
             <h1 className=' text-xl font-semibold'>{characterName}</h1>
-            <h3 className=' text-sm font-medium'>{data?.character}</h3>
+            <h3 className=' text-sm font-medium'>{ data?.character || data?.job}</h3>
             <h3 className=' text-sm font-medium text-muted-foreground mt-2'>{data?.known_for_department}</h3>
           </div>
       </div>
