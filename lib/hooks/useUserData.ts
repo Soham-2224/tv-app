d7ecc761f2407e530c04ae73de09aae6a1f4f2fc
soham-2224 <sohambhosle26@gmail.com>
2024-01-07 16:35:03 +0530

    network connection checked for favourite

--- lib/hooks/useUserData.ts
+++ lib/hooks/useUserData.ts
@@ -2,11 +2,18 @@ import { useState, useEffect, useContext } from "react"
 
 import { HankoContext } from "@/components/auth/HankoProvider"
 import useUserStore from "@/store/useUser"
+import { toast } from 'sonner'
 
 export type HankoUser = {
     email: string
     loading: boolean
     error: string | null
+}
+
+type Errors = {
+    noError: Omit<HankoUser, "email">,
+    loggedOut: Omit<HankoUser, "email">,
+    connectionError: Omit<HankoUser, "email">,
 }
 
 export function useUserData(): HankoUser {
@@ -19,25 +26,35 @@ export function useUserData(): HankoUser {
     const storedUser = useUserStore((state) => state.email)
     const updateUser = useUserStore((state) => state.setUser)
 
+    const ErrorMessages : Errors = {
+        noError: { loading: false, error: null },
+        loggedOut: { loading: false, error: "logged out" },
+        connectionError: { loading: false, error: "Error connecting" }
+    }
+
     useEffect(() => {
+
+        if (!navigator.onLine) {
+             toast.error("Check your internet connection")
+        }
 
         if (!hanko) return
         
         if (!hanko.session.isValid()) {
-            setUserState({ email: "", loading: false, error: "logged out" })
-            updateUser({ email: "", loading: false, error: "logged out" })
+            setUserState({ email: "", ...ErrorMessages.loggedOut })
+            updateUser({ email: "", ...ErrorMessages.loggedOut })
             return 
         }
         
         if (storedUser.length) {
-            return setUserState({ email: storedUser, loading: false, error: null })
+            return setUserState({ email: storedUser, ...ErrorMessages.noError })
         }
 
         hanko.user
             ?.getCurrent()
             .then(({ email }) => {
-                updateUser({ email, loading: false, error: null })
-                setUserState({  email, loading: false, error: null })
+                updateUser({ email, ...ErrorMessages.noError })
+                setUserState({  email, ...ErrorMessages.noError })
             })
             .catch((error) => {
                 setUserState((prevState) => ({ ...prevState, loading: false, error }))
