e3a70751509a149a190acfd84c9be748695fc264
soham-2224 <sohambhosle26@gmail.com>
2024-01-10 19:49:02 +0530

    postercolumn suspense, fetching, components done

---
+++ components/shared/RightSidebar/SinglePosterColumn.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+
+// --constants--
+import { GENRES } from "@/lib/constants"
+
+// --components--
+import RatingBadge from "@/components/shared/RatingBadge"
+import { Movie } from "@/typings"
+import getImagePath from "@/lib/getImagePath"
+
+const SinglePosterColumn = ({ data } : {data : Movie | undefined}) => {
+    const filteredGenres = GENRES.filter((genre) => data?.genre_ids?.includes(genre.id))
+        .map((genre) => genre.name)
+        .join(", ")
+
+    return (
+        <Link
+            href={`/movies/${data?.id}`}
+            className="flex items-center gap-4 max-w-full h-auto group"
+        >
+            <Image
+                src={getImagePath({ data, isPoster: true })}
+                blurDataURL="/assets/no-image-placeholder.png"
+                placeholder="blur"
+                width={70}
+                height={105}
+                alt={data?.title || ""}
+                className="rounded-md"
+            />
+            <div className="flex flex-col justify-between gap-4">
+                <div className="">
+                    <h1 className=" line-clamp-2 group-hover:text-primary text-sm font-semibold">{data?.title}</h1>
+                    <p className="line-clamp-1 text-xs font-normal text-muted-foreground">{filteredGenres}</p>
+                </div>
+                <RatingBadge rating={data?.vote_average || 0} />
+            </div>
+        </Link>
+    )
+}
+
+export default SinglePosterColumn
--- components/shared/RightSidebar/index.tsx
+++ components/shared/RightSidebar/index.tsx
@@ -1,54 +1,19 @@
-import React from "react"
+import React, { Suspense } from "react"
 import Link from "next/link"
 
 // --components--
-import PosterColumn from "./PosterColumn"
 import { Button } from "@/components/ui/button"
+import PosterColumns from "./PosterColumns"
+import PosterColumnSkeleton from "../Skeletons/PosterColumnSkeleton"
 
-const MOVIE_API_RES = [
-    {
-        id: 466420,
-        poster_path: "",
-        title: "Killers of the Flower Moon",
-        genre_ids: [80, 18, 36],
-        vote_average: 7.622
-    },
-    {
-        id: 466420,
-        poster_path: "",
-        title: "Killers of the Flower Moon",
-        genre_ids: [80, 18, 36],
-        vote_average: 7.622
-    },
-    {
-        id: 466420,
-        poster_path: "",
-        title: "Killers of the Flower Moon",
-        genre_ids: [80, 18, 36],
-        vote_average: 7.622
-    },
-    {
-        id: 466420,
-        poster_path: "",
-        title: "Killers of the Flower Moon",
-        genre_ids: [80, 18, 36],
-        vote_average: 7.622
-    }
-]
-
-const RightSidebar = () => {
+export default function RightSidebar() {
     return (
         <div className=" hidden xl:block py-6 px-4 w-1/5 max-w-[250px] h-full bg-background border-l border-border">
             <div>
                 <h1 className="title-bold">Popular movies</h1>
-                <div className="flex flex-col gap-3 mt-4">
-                    {MOVIE_API_RES?.slice(0, 4).map((movie) => (
-                        <PosterColumn
-                            key={movie.id}
-                            data={movie}
-                        />
-                    ))}
-                </div>
+                <Suspense fallback={<PosterColumnSkeleton />}>
+                    <PosterColumns />
+                </Suspense>
                 <Link href="/">
                     <Button className=" block mx-auto mt-4 px-10">See More</Button>
                 </Link>
@@ -56,5 +21,3 @@ const RightSidebar = () => {
         </div>
     )
 }
-
-export default RightSidebar
