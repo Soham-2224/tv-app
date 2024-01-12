452e5b883581c6c9dab9764db0f20447c5099163
soham-2224 <sohambhosle26@gmail.com>
2024-01-12 23:11:34 +0530

    favourite page in tv

--- app/(root)/tv/favourite/page.tsx
+++ app/(root)/tv/favourite/page.tsx
@@ -1,7 +1,40 @@
+"use client"
+
+// --components--
+import CarouselCard from "@/components/shared/CarouselCard"
+import CenterLoginBtn from "@/components/shared/CenterLoginBtn"
+import Loading from "./loading"
+
+// --zustand--
+import useUserStore from "@/store/useUser"
+
+// --types--
+import { Favourite } from "@/typings"
+
+// --localstorage--
+import useLocalStorage from "@rehooks/local-storage"
+
 export default function Page() {
+    const { email, loading, error } = useUserStore((state) => state)
+    const [likedShows] = useLocalStorage<Favourite[]>("likedTvShows")
+
+    if (loading) return <Loading />
+
+    if (error) return <CenterLoginBtn />
+
     return (
-        <>
-            <h1>Hello</h1>
-        </>
+        <main className=" p-4 md:p-6">
+            <h1 className="title-bold">Favourite TV Shows</h1>
+            <div className="card-grid">
+                {likedShows?.reverse().map((movie) => (
+                    <CarouselCard
+                        isTv
+                        withLikeIcon
+                        key={movie.id}
+                        data={movie}
+                    />
+                ))}
+            </div>
+        </main>
     )
 }
