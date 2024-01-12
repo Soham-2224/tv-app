62307e5dce73584579b23e5eca2182d1d88c3842
soham-2224 <sohambhosle26@gmail.com>
2024-01-12 09:50:28 +0530

    types updated to support tv details and clean code

--- typings.ts
+++ typings.ts
@@ -34,15 +34,6 @@ export type SearchResults = {
     total_results: number
 }
 
-export type Genre = {
-    id: number
-    name: string
-}
-
-export type Genres = {
-    genres: Genre[]
-}
-
 export type Favourite = Pick<
     CommonMovieTv,
     | "backdrop_path"
@@ -55,6 +46,47 @@ export type Favourite = Pick<
     | "name"
     | "first_air_date"
 >
+
+export type Genre = {
+    id: number
+    name: string
+}
+
+export type Genres = {
+    genres: Genre[]
+}
+
+export type MovieOrTv = "movie" | "tv"
+
+export type Video = {
+    iso_639_1: string
+    iso_3166_1: string
+    name: string
+    key: string
+    site: string
+    size: number
+    type: string
+    official: boolean
+    published_at: string
+    id: string
+}
+
+export type Review = {
+    author: string
+    author_details: {
+        name: string
+        username: string
+        avatar_path: string | null
+        rating: number
+    }
+    content: string
+    created_at: string
+    id: string
+    updated_at: string
+    url: string
+}
+
+// ---------------Single-Movie-Details---------------------
 
 export type SingleMovieDetail = Omit<Movie, "genre_ids"> & {
     belongs_to_collection?: BelongsToCollection | null
@@ -125,32 +157,82 @@ export type SpokenLanguage = {
     name: string
 }
 
-export type Video = {
-    iso_639_1: string
+
+// ---------------Single-TV-Details---------------------
+
+export type SingleTvDetail = Omit<TV, "genre_ids"> & {
+    created_by?: CreatedByEntity[] | null
+    episode_run_time?: null[] | null
+    genres?: Genres[] | null
+    in_production: boolean
+    languages?: string[] | null
+    last_air_date: string
+    last_episode_to_air: LastEpisodeToAirOrNextEpisodeToAir
+    next_episode_to_air: LastEpisodeToAirOrNextEpisodeToAir
+    networks?: NetworksEntityOrProductionCompaniesEntity[] | null
+    number_of_episodes: number
+    number_of_seasons: number
+    origin_country?: string[] | null
+    original_name: string
+    production_companies?: NetworksEntityOrProductionCompaniesEntity[] | null
+    production_countries?: ProductionCountriesEntity[] | null
+    seasons?: SeasonsEntity[] | null
+    spoken_languages?: SpokenLanguagesEntity[] | null
+    status: string
+    tagline: string
+    type: string
+    videos?: {
+        results: Video[]
+    }
+    credits: {
+        cast: Actor[]
+        crew: Crew[]
+    }
+}
+export type CreatedByEntity = {
+    id: number
+    credit_id: string
+    name: string
+    gender: number
+    profile_path: string
+}
+export type LastEpisodeToAirOrNextEpisodeToAir = {
+    id: number
+    name: string
+    overview: string
+    vote_average: number
+    vote_count: number
+    air_date: string
+    episode_number: number
+    episode_type: string
+    production_code: string
+    runtime?: null
+    season_number: number
+    show_id: number
+    still_path?: null
+}
+export type NetworksEntityOrProductionCompaniesEntity = {
+    id: number
+    logo_path: string
+    name: string
+    origin_country: string
+}
+export type ProductionCountriesEntity = {
     iso_3166_1: string
     name: string
-    key: string
-    site: string
-    size: number
-    type: string
-    official: boolean
-    published_at: string
-    id: string
 }
-
-export type Review = {
-    author: string
-    author_details: {
-        name: string
-        username: string
-        avatar_path: string | null
-        rating: number
-    }
-    content: string
-    created_at: string
-    id: string
-    updated_at: string
-    url: string
+export type SeasonsEntity = {
+    air_date: string
+    episode_count: number
+    id: number
+    name: string
+    overview: string
+    poster_path: string
+    season_number: number
+    vote_average: number
 }
-
-export type MovieOrTv = "movie" | "tv"
+export type SpokenLanguagesEntity = {
+    english_name: string
+    iso_639_1: string
+    name: string
+}
