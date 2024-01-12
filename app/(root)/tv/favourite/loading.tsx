8923603d00a0977d0b32885bf829217347792cfd
soham-2224 <sohambhosle26@gmail.com>
2024-01-12 23:10:59 +0530

    created skeleton for favourite

---
+++ app/(root)/tv/favourite/loading.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+    return (
+        <main className=" p-4 md:p-6">
+            <h1 className="title-bold">Loading...</h1>
+            <div className="card-grid">
+                {Array.from({length: 8}, (_, index) => ({ id: index + 1 })).map((movie) => (
+                    <Skeleton key={movie.id} className=" w-full h-64" />
+                ))}
+            </div>
+        </main>
+    )
+}
