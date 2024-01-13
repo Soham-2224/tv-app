a7ecdad6d3f19d27d59be41b4d707d5f875f2e51
soham-2224 <sohambhosle26@gmail.com>
2024-01-13 15:40:44 +0530

    auth page styles

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,23 @@
+import Link from "next/link"
 import dynamic from "next/dynamic"
 const HankoAuth = dynamic(() => import("@/components/auth/HankoAuth"), { ssr: false })
 
+// --components--
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+
 export default function LoginPage() {
-    return <HankoAuth />
+    return (
+        <div className=" w-full h-screen grid place-items-center">
+            <div className=" flex flex-col gap-4">
+                <HankoAuth />
+                <div className="flex flex-col gap-4 items-center px-8">
+                <Separator className=" w-full" />
+                <Link href="/movies" className="w-full">
+                    <Button className="w-full">Skip login</Button>
+                </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
